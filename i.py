6c1283b88c072c5557b3b87ed27aaae975cd93a9
Rawi Daniel <rawidaniel96@gmail.com>
2022-11-29 17:33:14 +0300

    update

--- i.py
+++ i.py
@@ -16,13 +16,17 @@
 # print(ho.id)
 
 #HBNB_MYSQL_USER=hbnb_addis_review HBNB_MYSQL_PWD=addisreview HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_addis_review_db HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5000 python -m api.v1.app
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
     
-def allowed_file(filename):
-        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-print(allowed_file("rawi.png"))
+# print(allowed_file("rawi.png"))
 
-ra = "rawera.jpeg".rsplit(".", 1)
-print(ra)
-print(ra[1])
+# ra = "rawera.jpeg".rsplit(".", 1)
+# print(ra)
+# print(ra[1])
+
+import datetime
+date = datetime.datetime.utcnow()
+print("{} {}".format(date.strftime("%d"), date.strftime("%b")))
