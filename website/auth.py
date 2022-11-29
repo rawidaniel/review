e968cccb23694105940c3487d8fe5229bcdab1fa
Rawi Daniel <rawidaniel96@gmail.com>
2022-11-29 15:09:20 +0300

    update

--- website/auth.py
+++ website/auth.py
@@ -13,7 +13,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
-`x`    """Reterive login page"""
+    """Reterive login page"""
     if current_user.is_authenticated:
         return redirect(url_for("main.restaurants"))
     return render_template("login.html")
